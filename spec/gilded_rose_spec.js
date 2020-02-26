959cf1c9dfd9094d31b19d0d9f9b9ae156b84dae
Squareben1 <bengittins@live.com>
2020-02-26 17:10:57 +0000

    lowers sellin value

---
+++ spec/gilded_rose_spec.js
@@ -0,0 +1,18 @@
+// var {Shop, Item} = require('../src/gilded_rose.js');
+
+describe("Gilded Rose", function() {
+  describe("updateQuality", function() {
+    function ItemDouble(name, sellIn, quality) {
+      this.name = name;
+      this.sellIn = sellIn;
+      this.quality = quality;
+    }
+
+    it("should lower Sellin value", function() {
+      const gildedRose = new Shop([ new ItemDouble("potion", 1, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+    })
+
+  })
+});
