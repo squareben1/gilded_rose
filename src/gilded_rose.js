9fafd8a9aa3d19a86e140f4e858c64edf04bb82d
Squareben1 <bengittins@live.com>
2020-02-27 15:02:35 +0000

    extracted specialQuality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,15 +11,20 @@ class Shop {
     this.items = items;
   }
 
-  backstagePasses(i) {
-    if (this.items[i].sellIn < 11) {
-      if (this.items[i].quality < 50) {
-        this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 2 if sellIn <11
-      }
+  backstagePassesQuality(i) {
+    if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 2 if sellIn <11
     }
-    if (this.items[i].sellIn < 6) {
-      if (this.items[i].quality < 50) {
-        this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 3 if sellIn <6
+    if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 3 if sellIn <6
+    }
+  }
+
+  specialQuality(i) {
+    if (this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 1
+      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { //PASSES qality
+        this.backstagePassesQuality(i)
       }
     }
   }
@@ -33,22 +38,13 @@ class Shop {
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { //PASSES qality
-            this.backstagePasses(i)
-            // if (this.items[i].sellIn < 11) {
-            //   if (this.items[i].quality < 50) {
-            //     this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 2 if sellIn <11
-            //   }
-            // }
-            // if (this.items[i].sellIn < 6) {
-            //   if (this.items[i].quality < 50) {
-            //     this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 3 if sellIn <6
-            //   }
-            // }
-          }
-        }
+        this.specialQuality(i)
+        // if (this.items[i].quality < 50) {
+        //   this.items[i].quality = this.items[i].quality + 1; // increase QUALITY by 1
+        //   if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { //PASSES qality
+        //     this.backstagePassesQuality(i)
+        //   }
+        // }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1; // DEcrease SELLIN by 1
